ab01e1abb176e0e23192af80f98606eb79db4360
Theint Kyaw <theintthinzark21@gmail.com>
2017-07-14 18:43:48 +0000

    created Pages controller. Added four pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+#This page contains all the code for any page inside of /pages
+class PagesController < ApplicationController
+  
+  #backend code for pages/index
+  def index
+  end
+
+  #backend code for pages/home
+  def home
+  end
+
+  #backend code for pages/profile
+  def profile
+  end
+
+  #backend code for pages/explore
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  #Define routes for Pages
+  root 'pages#index'
+  #get 'pages/index'
+  get '/home' => 'pages#home'
+
+  get '/profile' => 'pages#profile'
+
+  get '/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
